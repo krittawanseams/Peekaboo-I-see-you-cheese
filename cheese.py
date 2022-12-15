54ed580a5aff0bf05be4ed84ff7b8eb936788763
krittawanseams <115057049+krittawanseams@users.noreply.github.com>
2022-12-15 21:00:27 +0700

    update

--- cheese.py
+++ cheese.py
@@ -2,14 +2,22 @@ from turtle import Turtle
 
 
 class Cheese(Turtle):
-    def __init__(self, x, y):
+    def __init__(self, x, y, cheese=1):
         Turtle.__init__(self)
         self.shape('cheese2.gif')
         self.color('yellow')
         self.penup()
         self.speed(0)
-        self.cheese = 1
+        self.cheese = cheese
         self.goto(x, y)
+
+    @property
+    def cheese(self):
+        return self.cheese
+
+    @cheese.setter
+    def cheese(self, cheese):
+        self.cheese = cheese
 
     def destroy(self):
         self.goto(1000, 1000)
